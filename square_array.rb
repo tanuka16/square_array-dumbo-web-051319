8b211856bdce52650af7636004170b5bdb02ca43
Tanuka Das <das.tanuka@yahoo.com>
2019-04-27 00:59:53 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,16 +1,6 @@
 def square_array(array)
   # your code here
-  newArray=[]
-  array.each do |numbers|
 
-   numbers=numbers**2 # square
-  newArray<<numbers # adding to the newArray
-  end     #end for do
-  newArray # call
-  end
-
-
-=begin
 new_array = []
   array.each do |n|
     n = n**2;
@@ -18,5 +8,5 @@ new_array = []
   end
   new_array
 end
-=end
+
 #taking each item 1 by 1 from the object array; give the taken item a refence name "n"
