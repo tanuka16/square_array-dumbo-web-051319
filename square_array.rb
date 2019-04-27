c696c94221b5f52eea5527a82654e4b936738b6f
Tanuka Das <das.tanuka@yahoo.com>
2019-04-27 00:44:14 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,6 +1,7 @@
 def square_array(array)
   # your code here
   count  = 1
-  array.each {|n| puts n**2; }            #taking each item 1 by 1 from the object array; give the taken item a refence name "n"
-
+  array.each do |n|              #taking each item 1 by 1 from the object array; give the taken item a refence name "n"
+    puts n**2;
+  end
 end
