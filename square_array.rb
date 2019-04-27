e2dcf8015ce354706a05ec7114dec2fc67c36272
Tanuka Das <das.tanuka@yahoo.com>
2019-04-27 01:04:32 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -8,3 +8,12 @@ new_array = []
   end                                      # end for d
   new_array                                 #call the array list
 end
+
+
+new_array = []
+  array.collect do |n|                        #taking each item 1 by 1 from the object array; give the taken item a refence name "n"
+    n = n**2;                               #square
+    new_array<<n                           # adding n to the new array
+  end                                      # end for d
+  new_array                                 #call the array list
+end
