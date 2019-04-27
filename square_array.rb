e9393bee90e5b8adc748038878da57d5d70a7d86
Tanuka Das <das.tanuka@yahoo.com>
2019-04-27 01:04:40 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -2,15 +2,6 @@ def square_array(array)
   # your code here
 
 new_array = []
-  array.each do |n|                        #taking each item 1 by 1 from the object array; give the taken item a refence name "n"
-    n = n**2;                               #square
-    new_array<<n                           # adding n to the new array
-  end                                      # end for d
-  new_array                                 #call the array list
-end
-
-
-new_array = []
   array.collect do |n|                        #taking each item 1 by 1 from the object array; give the taken item a refence name "n"
     n = n**2;                               #square
     new_array<<n                           # adding n to the new array
